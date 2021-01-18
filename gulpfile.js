e4dad2825d3dfe04f1ec53b12fd719d83b46778f
Sergei <westberrie@gmail.com>
2021-01-18 19:21:13 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const terser = require('gulp-terser');
+
+gulp.task('server', function () {
+  browserSync({
+    server: {
+      baseDir: 'docs',
+    },
+  });
+
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('styles', function () {
+  return gulp
+    .src('src/sass/**/*.+(scss|sass)')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min', prefix: '' }))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('docs/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+  gulp.watch('src/*.html').on('change', gulp.parallel('html'));
+  gulp.watch('src/js/**/*.js').on('change', gulp.parallel('scripts'));
+  gulp.watch('src/fonts/**/*').on('all', gulp.parallel('fonts'));
+  gulp.watch('src/icons/**/*').on('all', gulp.parallel('icons'));
+  gulp.watch('src/img/**/*').on('all', gulp.parallel('images'));
+});
+
+gulp.task('html', function () {
+  return gulp
+    .src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('docs/'));
+});
+
+gulp.task('scripts', function () {
+  return gulp
+    .src('src/js/**/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('docs/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('fonts', function () {
+  return gulp
+    .src('src/fonts/**/*')
+    .pipe(gulp.dest('docs/fonts'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('icons', function () {
+  return gulp
+    .src('src/icons/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('docs/icons'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', function () {
+  return gulp
+    .src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('docs/img'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task(
+  'default',
+  gulp.parallel(
+    'watch',
+    'server',
+    'styles',
+    'scripts',
+    'fonts',
+    'icons',
+    'html',
+    'images'
+  )
+);
